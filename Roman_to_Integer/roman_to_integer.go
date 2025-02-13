fea83a21341b66f94367f93d7a14b3583715e5b7
Kasra Nadri <kasra.n2018@gmail.com>
2025-02-13 14:58:19 +0330

    started on roman_to _integer

---
+++ Roman_to_Integer/roman_to_integer.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+
+}
+
+func romanToInt(s string) int {
+	mapOfRomanValues := make(map[string]int)
+	mapOfRomanValues["I"] = 1
+	mapOfRomanValues["V"] = 5
+	mapOfRomanValues["X"] = 10
+	mapOfRomanValues["L"] = 50
+	mapOfRomanValues["C"] = 100
+	mapOfRomanValues["D"] = 500
+	mapOfRomanValues["M"] = 100
+}
